80c30d87ae9fb5dc082754d2928406c30e15c1b2
Laurenz Guevara <laurenzguevara@outlook.com>
2024-06-13 00:26:50 +0100

    feat: Show select restaurant bar on specific layouts

---
+++ src/app/dashboard/(no-select-restaurant-bar)/layout.tsx
@@ -0,0 +1,21 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { isAuthenticated } = getKindeServerSession();
+  return (await isAuthenticated()) ? (
+    <>{children}</>
+  ) : (
+    <MaxWidthWrapper>
+      <div className="py-4">
+        This page is protected, please&nbsp;
+        <LoginLink className="text-green-600">Login</LoginLink> to view it.
+      </div>
+    </MaxWidthWrapper>
+  );
+}
